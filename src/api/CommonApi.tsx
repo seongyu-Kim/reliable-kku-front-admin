a5b1b6092176886521c7d91053fb4cf1f2ef2687
seongyu <seongyu1066@naver.com>
2023-11-10 16:41:36 +0900

    feat: BASE_API 추가

---
+++ src/api/CommonApi.tsx
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import {MMKVLoader} from 'react-native-mmkv-storage';
+
+const MMKV = new MMKVLoader().initialize();
+
+export const BASE_API = axios.create({
+  // baseURL: 'https://dev.deunku.com',
+  responseType: 'json',
+  withCredentials: true,
+});
+
+BASE_API.interceptors.request.use(async config => {
+  const accessToken = await MMKV.getStringAsync('accessToken');
+
+  console.log(accessToken);
+  if (accessToken) {
+    config.headers.Authorization = accessToken;
+  }
+
+  return config;
+});
