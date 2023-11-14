90c126f24c84a21323975163c752569a903f900d
seongyu <seongyu1066@naver.com>
2023-11-14 16:53:06 +0900

    feat: sse 수정 및 주문 시 소리 구현

--- src/components/pages/order-list/order-list-layout/OrderListLayout.tsx
+++ src/components/pages/order-list/order-list-layout/OrderListLayout.tsx
@@ -1,48 +1,21 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {FlatList} from 'react-native';
 import * as styles from './OrderListLayout.styles';
 import WaitingOrderItem from '../waiting-order-item/WaitingOrderItem';
 import SiteOrderBtn from '../../on-site-order/site-order-btn/SiteOrderBtn';
 import ReceiptOrderItem from '../receipt-order-item/ReceiptOrderItem';
 import CompleteOrderItem from '../complete-order-item/CompleteOrderItem';
-import axios from 'axios';
 import {BASE_API} from '../../../../api/CommonApi';
-import {useFocusEffect} from '@react-navigation/native';
 import EventSource, {EventSourceListener} from 'react-native-sse';
-
-interface SSEProps {
-  orderStatus:
-    | 'WAIT'
-    | 'COOKING'
-    | 'PICKUP'
-    | 'FINISH'
-    | 'CANCELED'
-    | 'NOT_TAKE';
-}
+import Sound from 'react-native-sound';
 
 const OrderListLayout: React.FC<{
   isSideMenuVisible: boolean;
   selectedTopMenu: string;
 }> = ({isSideMenuVisible, selectedTopMenu}) => {
   //
-  const es = useRef<EventSource | null>(null);
   const [orders, setOrders] = useState<Order[]>([]);
-  // const [orderStatus, setOrderStatus] = useState<
-  //   'WAIT' | 'COOKING' | 'PICKUP' | 'FINISH' | 'CANCELED' | 'NOT_TAKE'
-  // >('WAIT');
-
-  // const renderItemComponent = () => {
-  //   switch (orderStatus) {
-  //     case 'WAIT':
-  //       return <WaitingOrderItem item={waitingOrderItem} />;
-  //     case 'COOKING':
-  //       return <ReceiptOrderItem item={receiptOrderItem} />;
-  //     case 'FINISH':
-  //       return <CompleteOrderItem item={completeOrderItem} />;
-  //     default:
-  //       return null;
-  //   }
-  // };
+  const [isClicked, setIsClicked] = useState(false);
 
   let renderItemComponent: any;
   let orderStatus: string = '';
@@ -50,12 +23,15 @@ const OrderListLayout: React.FC<{
   if (selectedTopMenu === '대기') {
     renderItemComponent = WaitingOrderItem;
     orderStatus = 'WAIT';
+    // setOrderStatus('WAIT');
   } else if (selectedTopMenu === '접수') {
     renderItemComponent = ReceiptOrderItem;
     orderStatus = 'COOKING';
+    // setOrderStatus('COOKING');
   } else if (selectedTopMenu === '완료') {
     renderItemComponent = CompleteOrderItem;
     orderStatus = 'FINISH';
+    // setOrderStatus('FINISH');
   }
 
   interface Order {
@@ -74,68 +50,70 @@ const OrderListLayout: React.FC<{
     count: number;
   }
 
-  // useFocusEffect(() => {
-  //   BASE_API.get(
-  //     `https://dev.deunku.com/api/v1/admin/orders?orderStatus=${orderStatus}`,
-  //   )
-  //     .then(response => {
-  //       setOrders(response.data);
-  //       console.log(response.data);
-  //       console.log(
-  //         '@@@@@@@@@@@@ㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇ@@@@@@@@@@@@@@@',
-  //       );
-  //     })
-  //     .catch(error => {
-  //       console.error('Error fetching orders(orderListLayout):', error);
-  //     });
-  // });
+  const playSound = () => {
+    const sound = new Sound(
+      require('../../../../assets/sound/alram.mp3'),
+      error => {
+        if (error) {
+          console.error('Failed to load the sound', error);
+          return;
+        }
+        sound.play(success => {
+          if (success) {
+            console.log('Sound played successfully');
+          } else {
+            console.error('Failed to play the sound');
+          }
+        });
+      },
+    );
+  };
 
   //SSE
-  // useEffect(() => {
-  //   es.current = new EventSource(
-  //     'https://dev.deunku.com/api/v1/admin/order/sse/connect',
-  //     {
-  //       withCredentials: true,
-  //       pollingInterval: 3000,
-  //       timeout: 0,
-  //     },
-  //   );
-  // }, []);
+  useEffect(() => {
+    const es = new EventSource(
+      'https://dev.deunku.com/api/v1/admin/order/sse/connect',
+      {
+        timeout: 0,
+        pollingInterval: 1000,
+      },
+    );
 
-  //SSE
-  // useEffect(() => {
-  //   if (es.current) {
-  //     const listener: EventSourceListener = event => {
-  //       if (event.type === 'open') {
-  //       } else if (event.type === 'message') {
-  //         let data: SSEProps;
-  //
-  //         if (event.data != null) {
-  //           data = JSON.parse(event.data);
-  //           console.log('===============================', data);
-  //           // setOrderStatus(data.orderStatus);
-  //         }
-  //       } else if (event.type === 'error') {
-  //       } else if (event.type === 'exception') {
-  //       }
-  //     };
-  //
-  //     es.current.addEventListener('open', listener);
-  //     es.current.addEventListener('message', listener);
-  //     es.current.addEventListener('error', listener);
-  //
-  //     return () => {
-  //       if (es.current) {
-  //         es.current.removeAllEventListeners();
-  //         es.current.close();
-  //       }
-  //     };
-  //   }
-  // }, []);
+    const listener: EventSourceListener = event => {
+      if (event.type === 'open') {
+        console.log('open!!!!!!!!!!!!!!!!!!!!!!!');
+      } else if (event.type === 'message') {
+        console.log('event.data>>>>>>>>', event.data);
+        let data: Order;
 
-  const [isClicked, setIsClicked] = useState(false);
+        if (event.data != null) {
+          data = JSON.parse(event.data);
+          console.log('data!!!!!!!!!!!!!!!!!!!!!!!!!!!!:', data);
+          if (orderStatus === 'WAIT') {
+            setOrders([data, ...orders]);
+            playSound();
+          } else {
+            //asd
+          }
+        }
+      } else if (event.type === 'error') {
+        console.log('SSE connection error');
+      } else if (event.type === 'exception') {
+        console.log('SSE connection exception');
+      }
+    };
 
-  //주문
+    es.addEventListener('open', listener);
+    es.addEventListener('message', listener);
+    es.addEventListener('error', listener);
+
+    return () => {
+      es.removeAllEventListeners();
+      es.close();
+    };
+  }, []);
+
+  // 주문;
   useEffect(() => {
     const fetchOrders = async () => {
       await BASE_API.get(
@@ -144,9 +122,7 @@ const OrderListLayout: React.FC<{
         .then(response => {
           setOrders(response.data);
           console.log(response.data);
-          console.log(
-            '@@@@@@@@@@ㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁ@@@@@@@@@@@@@@@@@',
-          );
+          console.log('@@@@@@@@');
         })
         .catch(error => {
           console.error('Error fetching orders(orderListLayout):', error);
@@ -154,7 +130,7 @@ const OrderListLayout: React.FC<{
     };
 
     fetchOrders();
-  }, [orderStatus, isClicked]);
+  }, [orderStatus]);
 
   const totalBunCount = orders.reduce((acc, order) => {
     const bunCount = order.menuResponse.reduce((menuAcc, menu) => {
@@ -167,7 +143,7 @@ const OrderListLayout: React.FC<{
     <>
       <styles.OrderView>
         <FlatList
-          data={orders.reverse()}
+          data={orders}
           renderItem={item => {
             const RenderItemComponent = renderItemComponent;
             return (
